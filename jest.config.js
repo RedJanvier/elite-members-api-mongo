f06db128c62343a47d65b5cd90602a931d4dd8ca
RedJanvier <janvierntwali@gmail.com>
2020-03-31 02:26:12 +0200

    configured travis for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   verbose: true,
+  transform: {
+    '^.+\\.[jt]sx?$': '<rootDir>/packages/babel-jest',
+  },
+  coverageReporters: ['json'],
+  reporters: [
+    [
+      'jest-junit',
+      { outputDirectory: 'reports/junit', outputName: 'js-test-results.xml' },
+    ],
+    [
+      'jest-silent-reporter',
+      { showPaths: true, showWarnings: true, useDots: true },
+    ],
+  ],
 };
