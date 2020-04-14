672649263b3905cc0097d2afa501c9fc1fa7e184
RedJanvier <janvierntwali@gmail.com>
2020-04-14 20:25:49 +0200

    bugfix(deepscan): null check
    on getSingle member controller response
    
    related to #10

--- api/controllers/members.js
+++ api/controllers/members.js
@@ -26,11 +26,11 @@ export const getAll = async (req, res, next) => {
 };
 export const getSingle = async (req, res, next) => {
   try {
-    const user = await Member.find({ _id: req.params.memberId }).select('_id ');
-    delete user[0].__v;
-    return res
-      .status(200)
-      .json({ success: true, data: user[0] || 'member not found!' });
+    const [member] = await Member.find({ _id: req.params.memberId }).select(
+      '_id '
+    );
+    delete member.__v;
+    return res.status(200).json({ success: true, data: member });
   } catch (err) {
     return res.status(500).json({ success: false, error: err.message });
   }
